61099bc0d6b9d468265c9d2b0f9d17d6f027c8ca
Matthew Teelucksingh <matthewteelucksingh@localhost.localdomain>
2020-04-14 00:40:14 -0400

    Documentation and Inital Base Class

--- src/pdf_ingestion_engine/financial_report_ingestion.py
+++ src/pdf_ingestion_engine/financial_report_ingestion.py
@@ -1,7 +1,40 @@
+# Importing PyPDF2 PDF management package:
 import PyPDF2 as p2
 
-# Opening the pdf file:
-pdf = open('tests/test_pdfs/ExxonMobil 2019 10-K Report.pdf', 'rb')
+
+# Creating class that is meant to represent the Financial Report PDF:
+class financial_report(object):
+    """
+    financial_report() object contains all the methods necessary to parse a
+    10-K or 10-Q Financial Report.
+
+    Critically most of the methods stored within this object are used to
+    construct instance variables and as such many of the methods are called within
+    the initalization of object and heavily make use of the PyPDF2 library.
+    This will primarily be used as a base class to be inherited for other
+    packages and methods.
+
+    Parameters
+    -----------
+
+    Methods
+    -----------
+
+    """
+
+    def __init__(self, file_path):
+
+    # Declaring key instance variables:
+    self.file_path = file_path
+
+
+    # Opening the pdf file:
+    pdf = open('tests/test_pdfs/ExxonMobil 2019 10-K Report.pdf', 'rb')
+
+    # TODO: Write the PDF PyPDF2 Object initalization as well as Password Decrypt.
+
+
+
 
 pdfread = p2.PdfFileReader(pdf)
 
